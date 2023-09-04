585009be9f456a413980aa809374ded279fb6557
balamurugan110799 <balamurugan99711@gmail.com>
2023-09-04 08:51:40 +0530

    auth route

--- src/App.js
+++ src/App.js
@@ -1,23 +1,74 @@
 import './App.css';
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { BrowserRouter, Route, Routes, Navigate } from 'react-router-dom';
 import Dashboard from './Pages/DashBoard';
 import Importance from './Pages/Importance';
 import "./Components/CSS/style.css"
 import ExtraExpensive from './Pages/ExtraExpensive';
 import Admin from './Pages/Admin';
+import Login from './Pages/Login';
+import PageNotFound from './Pages/PageNotFound';
 
 function App() {
+  console.log(sessionStorage.getItem("role"))
+  const auth = sessionStorage.getItem("auth")
+  const user = sessionStorage.getItem("role")
+  const admin = sessionStorage.getItem("role")
+  const superadmin = sessionStorage.getItem("role")
+
+  console.log(auth !== undefined || auth !== null, "yeyydgy")
   return (
     <div>
       <BrowserRouter>
         <Routes>
-        <Route path="/" element={<Dashboard/>} />
-        <Route path="/admin" element={<Admin/>} />
-        <Route path="/importance" element={<Importance/>} />
-        <Route path="/extra-expensive" element={<ExtraExpensive/>} />
+          <Route path="/" element={<Login />} />
+
+          {sessionStorage.auth !== null && sessionStorage.auth !== undefined && user === "user" ?
+            <>
+              <Route path="/dashboard" element={<Dashboard />} />
+              <Route path="/importance" element={<Importance />} />
+              <Route path="/extra-expensive" element={<ExtraExpensive />} />
+            </>
+            :
+            <>
+              <Route path="/admin" element={<Admin />} />
+
+              <Route path="*" element={<Navigate replace to="/" />} />
+            </>
+          }
+
+
+{sessionStorage.auth !== null && sessionStorage.auth !== undefined && admin === "admin" ?
+            <>
+              <Route path="/dashboard" element={<Dashboard />} />
+              <Route path="/admin" element={<Admin />} />
+              <Route path="/extra-expensive" element={<ExtraExpensive />} />
+              <Route path="/importance" element={<Importance />} />
+
+            </>
+            :
+            <>
+              <Route path="*" element={<Navigate replace to="/" />} />
+            </>
+          }
+
+
+
+          {sessionStorage.auth !== null && sessionStorage.auth !== undefined && superadmin === "super-admin" ?
+            <>
+              <Route path="/dashboard" element={<Dashboard />} />
+              <Route path="/admin" element={<Admin />} />
+              <Route path="/extra-expensive" element={<ExtraExpensive />} />
+              <Route path="/importance" element={<Importance />} />
+            </>
+            :
+            <>
+              <Route path="*" element={<Navigate replace to="/" />} />
+            </>
+          }
+
+          <Route path="*" element={<PageNotFound />} />
         </Routes>
       </BrowserRouter>
-    
     </div>
   );
 }
--- src/Pages/DashBoard.js
+++ src/Pages/DashBoard.js
@@ -208,6 +208,78 @@ function Dashboard() {
                 </div>
                 <div className="w-[80%]">
                     <Layout>
+
+                    <table>
+                            <thead className="mb-2">
+                                <tr className="bg-primary text-[#fff]">
+                                    <td className="px-4 text-left py-2"> Importance</td>
+                                    <td className="px-4 text-left py-2">{currentdata[0].month}</td>
+                                    <td className="px-4 text-left py-2">{currentdata[1].month}</td>
+                                    <td className="px-4 text-left py-2">{currentdata[2].month}</td>
+                                    <td className="px-4 text-left py-2">{currentdata[3].month}</td>
+                                    <td className="px-4 text-left py-2">{currentdata[4].month}</td>
+                                    <td className="px-4 text-left py-2">{currentdata[5].month}</td>
+                                    <td className="px-4 text-left py-2">{currentdata[6].month}</td>
+                                    <td className="px-4 text-left py-2">{currentdata[7].month}</td>
+                                    <td className="px-4 text-left py-2">{currentdata[8].month}</td>
+                                    <td className="px-4 text-left py-2">{currentdata[9].month}</td>
+                                    <td className="px-4 text-left py-2">{currentdata[10].month}</td>
+                                    <td className="px-4 text-left py-2">{currentdata[11].month}</td>
+                                </tr>
+                            </thead>
+
+                         
+                            <tbody>
+                                <tr className="my-2">
+                                    <td className="px-4 text-left">Bike</td>
+                                    <td className="px-4 mt-1 center">{currentdata[0].bike === "done" ? <HiCheck className="text-whatsapp mx-auto"/> : currentdata[0].bike === null   ? <HiOutlineMinus className="text-error mx-auto"/>  : currentdata[0].bike === "pending" ? <HiOutlineRefresh className="text-alert mx-auto"/> : false}</td>
+                                    <td className="px-4 text-left">{currentdata[1].bike === "done" ? <HiCheck className="text-whatsapp mx-auto"/> : currentdata[1].bike === null   ? <HiOutlineMinus className="text-error mx-auto"/>  : currentdata[1].bike === "pending" ? <HiOutlineRefresh className="text-alert mx-auto"/> : false}</td>
+                                    <td className="px-4 text-left">{currentdata[2].bike === "done" ? <HiCheck className="text-whatsapp mx-auto"/> : currentdata[2].bike === null   ? <HiOutlineMinus className="text-error mx-auto"/>  : currentdata[2].bike === "pending" ? <HiOutlineRefresh className="text-alert mx-auto"/> : false}</td>
+                                    <td className="px-4 text-left">{currentdata[3].bike === "done" ? <HiCheck className="text-whatsapp mx-auto"/> : currentdata[3].bike === null   ? <HiOutlineMinus className="text-error mx-auto"/>  : currentdata[3].bike === "pending" ? <HiOutlineRefresh className="text-alert mx-auto"/> : false}</td>
+                                    <td className="px-4 text-left">{currentdata[4].bike === "done" ? <HiCheck className="text-whatsapp mx-auto"/> : currentdata[4].bike === null   ? <HiOutlineMinus className="text-error mx-auto"/>  : currentdata[4].bike === "pending" ? <HiOutlineRefresh className="text-alert mx-auto"/> : false}</td>
+                                    <td className="px-4 text-left">{currentdata[5].bike === "done" ? <HiCheck className="text-whatsapp mx-auto"/> : currentdata[5].bike === null   ? <HiOutlineMinus className="text-error mx-auto"/>  : currentdata[5].bike === "pending" ? <HiOutlineRefresh className="text-alert mx-auto"/> : false}</td>
+                                    <td className="px-4 text-left">{currentdata[6].bike === "done" ? <HiCheck className="text-whatsapp mx-auto"/> : currentdata[6].bike === null   ? <HiOutlineMinus className="text-error mx-auto"/>  : currentdata[6].bike === "pending" ? <HiOutlineRefresh className="text-alert mx-auto"/> : false}</td>
+                                    <td className="px-4 text-left">{currentdata[7].bike === "done" ? <HiCheck className="text-whatsapp mx-auto"/> : currentdata[7].bike === null   ? <HiOutlineMinus className="text-error mx-auto"/>  : currentdata[7].bike === "pending" ? <HiOutlineRefresh className="text-alert mx-auto"/> : false}</td>
+                                    <td className="px-4 text-left">{currentdata[8].bike === "done" ? <HiCheck className="text-whatsapp mx-auto" /> : currentdata[8].bike === null   ? <HiOutlineMinus className="text-error mx-auto"/>  : currentdata[8].bike === "pending" ? <HiOutlineRefresh className="text-alert mx-auto"/> : false}</td>
+                                    <td className="px-4 text-left">{currentdata[9].bike === "done" ? <HiCheck className="text-whatsapp mx-auto"/> : currentdata[9].bike === null   ? <HiOutlineMinus className="text-error mx-auto"/>  : currentdata[9].bike === "pending" ? <HiOutlineRefresh className="text-alert mx-auto"/> : false}</td>
+                                    <td className="px-4 text-left">{currentdata[10].bike === "done" ? <HiCheck className="text-whatsapp mx-auto"/> : currentdata[10].bike === null   ? <HiOutlineMinus className="text-error mx-auto"/>  : currentdata[10].bike === "pending" ? <HiOutlineRefresh className="text-alert mx-auto"/> : false}</td>
+                                    <td className="px-4 text-left">{currentdata[11].bike === "done" ? <HiCheck className="text-whatsapp mx-auto"/> : currentdata[11].bike === null   ? <HiOutlineMinus className="text-error mx-auto"/>  : currentdata[11].bike === "pending" ? <HiOutlineRefresh className="text-alert mx-auto"/> : false}</td>
+                                </tr>
+                                <tr className="my-2">
+                                    <td className="px-4 text-left">Repco</td>
+                                    <td className="px-4 mt-1 center">{currentdata[0].repco === "done" ? <HiCheck className="text-whatsapp mx-auto"/> : currentdata[0].repco === null   ? <HiOutlineMinus className="text-error mx-auto"/>  : currentdata[0].repco === "pending" ? <HiOutlineRefresh className="text-alert mx-auto"/> : false}</td>
+                                    <td className="px-4 text-left">{currentdata[1].repco === "done" ? <HiCheck className="text-whatsapp mx-auto"/> : currentdata[1].repco === null   ? <HiOutlineMinus className="text-error mx-auto"/>  : currentdata[1].repco === "pending" ? <HiOutlineRefresh className="text-alert mx-auto"/> : false}</td>
+                                    <td className="px-4 text-left">{currentdata[2].repco === "done" ? <HiCheck className="text-whatsapp mx-auto"/> : currentdata[2].repco === null   ? <HiOutlineMinus className="text-error mx-auto"/>  : currentdata[2].repco === "pending" ? <HiOutlineRefresh className="text-alert mx-auto"/> : false}</td>
+                                    <td className="px-4 text-left">{currentdata[3].repco === "done" ? <HiCheck className="text-whatsapp mx-auto"/> : currentdata[3].repco === null   ? <HiOutlineMinus className="text-error mx-auto"/>  : currentdata[3].repco === "pending" ? <HiOutlineRefresh className="text-alert mx-auto"/> : false}</td>
+                                    <td className="px-4 text-left">{currentdata[4].repco === "done" ? <HiCheck className="text-whatsapp mx-auto"/> : currentdata[4].repco === null   ? <HiOutlineMinus className="text-error mx-auto"/>  : currentdata[4].repco === "pending" ? <HiOutlineRefresh className="text-alert mx-auto"/> : false}</td>
+                                    <td className="px-4 text-left">{currentdata[5].repco === "done" ? <HiCheck className="text-whatsapp mx-auto"/> : currentdata[5].repco === null   ? <HiOutlineMinus className="text-error mx-auto"/>  : currentdata[5].repco === "pending" ? <HiOutlineRefresh className="text-alert mx-auto"/> : false}</td>
+                                    <td className="px-4 text-left">{currentdata[6].repco === "done" ? <HiCheck className="text-whatsapp mx-auto"/> : currentdata[6].repco === null   ? <HiOutlineMinus className="text-error mx-auto"/>  : currentdata[6].repco === "pending" ? <HiOutlineRefresh className="text-alert mx-auto"/> : false}</td>
+                                    <td className="px-4 text-left">{currentdata[7].repco === "done" ? <HiCheck className="text-whatsapp mx-auto"/> : currentdata[7].repco === null   ? <HiOutlineMinus className="text-error mx-auto"/>  : currentdata[7].repco === "pending" ? <HiOutlineRefresh className="text-alert mx-auto"/> : false}</td>
+                                    <td className="px-4 text-left">{currentdata[8].repco === "done" ? <HiCheck className="text-whatsapp mx-auto" /> : currentdata[8].repco === null   ? <HiOutlineMinus className="text-error mx-auto"/>  : currentdata[8].repco === "pending" ? <HiOutlineRefresh className="text-alert mx-auto"/> : false}</td>
+                                    <td className="px-4 text-left">{currentdata[9].repco === "done" ? <HiCheck className="text-whatsapp mx-auto"/> : currentdata[9].repco === null   ? <HiOutlineMinus className="text-error mx-auto"/>  : currentdata[9].repco === "pending" ? <HiOutlineRefresh className="text-alert mx-auto"/> : false}</td>
+                                    <td className="px-4 text-left">{currentdata[10].repco === "done" ? <HiCheck className="text-whatsapp mx-auto"/> : currentdata[10].repco === null   ? <HiOutlineMinus className="text-error mx-auto"/>  : currentdata[10].repco === "pending" ? <HiOutlineRefresh className="text-alert mx-auto"/> : false}</td>
+                                    <td className="px-4 text-left">{currentdata[11].repco === "done" ? <HiCheck className="text-whatsapp mx-auto"/> : currentdata[11].repco === null   ? <HiOutlineMinus className="text-error mx-auto"/>  : currentdata[11].repco === "pending" ? <HiOutlineRefresh className="text-alert mx-auto"/> : false}</td>
+                              
+                                </tr>
+                                <tr className="mb-1">
+                                    <td className="px-4 text-center">Due Amount</td>
+                                    <td className="px-4 flex justify-center">{currentdata[0].due_amount === "done" ? <HiCheck className="text-whatsapp mx-auto"/> : currentdata[0].due_amount === null   ? <HiOutlineMinus className="text-error mx-auto"/>  : currentdata[0].due_amount === "pending" ? <HiOutlineRefresh className="text-alert mx-auto"/> : false}</td>
+                                    <td className="px-4 relative">{currentdata[1].due_amount === "done" ? <HiCheck className="text-whatsapp mx-auto text-center"/> : currentdata[1].due_amount === null   ? <HiOutlineMinus className="text-error mx-auto ="/>  : currentdata[1].due_amount === "pending" ? <HiOutlineRefresh className="text-alert mx-auto text-center"/> : false}</td>
+                                    <td className="px-4 text-left">{currentdata[2].due_amount === "done" ? <HiCheck className="text-whatsapp mx-auto"/> : currentdata[2].due_amount === null   ? <HiOutlineMinus className="text-error mx-auto"/>  : currentdata[2].due_amount === "pending" ? <HiOutlineRefresh className="text-alert mx-auto"/> : false}</td>
+                                    <td className="px-4 text-left">{currentdata[3].due_amount === "done" ? <HiCheck className="text-whatsapp mx-auto"/> : currentdata[3].due_amount === null   ? <HiOutlineMinus className="text-error mx-auto"/>  : currentdata[3].due_amount === "pending" ? <HiOutlineRefresh className="text-alert mx-auto"/> : false}</td>
+                                    <td className="px-4 text-left">{currentdata[4].due_amount === "done" ? <HiCheck className="text-whatsapp mx-auto"/> : currentdata[4].due_amount === null   ? <HiOutlineMinus className="text-error mx-auto"/>  : currentdata[4].due_amount === "pending" ? <HiOutlineRefresh className="text-alert mx-auto"/> : false}</td>
+                                    <td className="px-4 text-left">{currentdata[5].due_amount === "done" ? <HiCheck className="text-whatsapp mx-auto"/> : currentdata[5].due_amount === null   ? <HiOutlineMinus className="text-error mx-auto"/>  : currentdata[5].due_amount === "pending" ? <HiOutlineRefresh className="text-alert mx-auto"/> : false}</td>
+                                    <td className="px-4 text-left">{currentdata[6].due_amount === "done" ? <HiCheck className="text-whatsapp mx-auto"/> : currentdata[6].due_amount === null   ? <HiOutlineMinus className="text-error mx-auto"/>  : currentdata[6].due_amount === "pending" ? <HiOutlineRefresh className="text-alert mx-auto"/> : false}</td>
+                                    <td className="px-4 text-left">{currentdata[7].due_amount === "done" ? <HiCheck className="text-whatsapp mx-auto"/> : currentdata[7].due_amount === null   ? <HiOutlineMinus className="text-error mx-auto"/>  : currentdata[7].due_amount === "pending" ? <HiOutlineRefresh className="text-alert mx-auto"/> : false}</td>
+                                    <td className="px-4 text-left">{currentdata[8].due_amount === "done" ? <HiCheck className="text-whatsapp mx-auto" /> : currentdata[8].due_amount === null   ? <HiOutlineMinus className="text-error mx-auto"/>  : currentdata[8].due_amount === "pending" ? <HiOutlineRefresh className="text-alert mx-auto"/> : false}</td>
+                                    <td className="px-4 text-left">{currentdata[9].due_amount === "done" ? <HiCheck className="text-whatsapp mx-auto"/> : currentdata[9].due_amount === null   ? <HiOutlineMinus className="text-error mx-auto"/>  : currentdata[9].due_amount === "pending" ? <HiOutlineRefresh className="text-alert mx-auto"/> : false}</td>
+                                    <td className="px-4 text-left">{currentdata[10].due_amount === "done" ? <HiCheck className="text-whatsapp mx-auto"/> : currentdata[10].due_amount === null   ? <HiOutlineMinus className="text-error mx-auto"/>  : currentdata[10].due_amount === "pending" ? <HiOutlineRefresh className="text-alert mx-auto"/> : false}</td>
+                                    <td className="px-4 text-left">{currentdata[11].due_amount === "done" ? <HiCheck className="text-whatsapp mx-auto"/> : currentdata[11].due_amount === null   ? <HiOutlineMinus className="text-error mx-auto"/>  : currentdata[11].due_amount === "pending" ? <HiOutlineRefresh className="text-alert mx-auto"/> : false}</td>
+                              
+                                </tr>
+                            </tbody>
+                           
+                        </table>
                         <table>
                             <thead className="mb-2">
                                 <tr className="bg-primary text-[#fff]">
