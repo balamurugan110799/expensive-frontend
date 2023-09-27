1567d00c83668e062e5931869f8f6f8206a211d8
balamurugan110799 <balamurugan99711@gmail.com>
2023-09-27 08:57:30 +0530

    estimate

--- src/Pages/House/Estimate.jsx
+++ src/Pages/House/Estimate.jsx
@@ -5,21 +5,32 @@ import PopUp from '../../Components/Popup'
 import Input from '../../Fields/Input'
 import axios from 'axios'
 import { HiOutlinePencilAlt, HiOutlineTrash } from "react-icons/hi";
+import DatePicker from "react-datepicker";
+import "react-datepicker/dist/react-datepicker.css";
+import { timeStamp } from '../Util/time'
+// import timeStamp from "../Util/time"
 
 export default function Estimate() {
 
   const [popUpState, setPopUpState] = useState(false)
   const [action, setAction] = useState("")
-  const [getEstimateData,setEstimateData]=useState()
+  const [getEstimateData, setEstimateData] = useState()
+  const [startDate, setStartDate] = useState(new Date());
 
   const [values, setValues] = useState({
     plan: "",
     amount: null,
+    date: null
   })
+
+  const setDate = (date, e) => {
+    setStartDate(date)
+  }
 
   const [errors, setErrors] = useState({
     plan: "",
     amount: null,
+    date: null
   })
   const [vaild, setVaild] = useState(false)
 
@@ -35,7 +46,7 @@ export default function Estimate() {
     })
   }
 
-  if(vaild===true){
+  if (vaild === true) {
     if (values.plan === "" || values.plan === undefined) {
       errors.plan = "Enter the Plan"
     } else {
@@ -49,47 +60,68 @@ export default function Estimate() {
     }
   }
 
-  const getEstimate = () =>{
+  const getEstimate = () => {
     axios.get("http://localhost:4000/api/getAllEstimate")
-    .then((res)=>{
-      console.log(res)
-      setEstimateData(res.data.data)
-    })
-    .catch((err)=>{
-        console.log(err)
-    })
-  }
-  useEffect(()=>{
-    getEstimate()
-  },[])
+      .then((res) => {
+        console.log(res)
+        res.data.getEst.forEach((el) => {
+          var date = new Date(el.date * 1000);
+          var months = ['01', '02', '03', '04', '05', '06', '07', '08', '09', '10', '11', '12'];
+          var year = date.getFullYear();
+          var month = months[date.getMonth()];
+          var date = date.getDate();
+          var time = date+ "/" +month + "/"+ year
+          el.showdate = time
+          console.log(time)
 
-  const handleEdit =(v,i) =>{
+          console.log(date)
+        })
+
+        setEstimateData(res.data.getEst)
+      })
+      .catch((err) => {
+        console.log(err)
+      })
+  }
+
+  console.log(getEstimateData)
+  useEffect(() => {
+    getEstimate()
+  }, [])
+  // console.log(getEstimateData,"Data")
+
+  const handleEdit = (v, i) => {
     setPopUpState(true)
     setValues(v)
   }
 
   const submitHandler = (e) => {
     e.preventDefault();
-    console.log(errors,values)
+    console.log(errors, values)
     setVaild(true)
-    if(errors.plan===true&& errors.amount===true){
+    alert("Hello")
+    var myDate = new Date(startDate); // Your timezone!
+    var myEpoch = myDate.getTime() / 1000.0;
+    values.date = myEpoch
+    // console.log(myEpoch)
+    if (errors.plan === true && errors.amount === true) {
       axios.post("http://localhost:4000/api/addEstimate", values)
-      .then((res)=>{
-        console.log(res)
-        setValues({
-          plan: "",
-          amount: null,
+        .then((res) => {
+          console.log(res)
+          setValues({
+            plan: "",
+            amount: null,
+          })
+          setPopUpState(false)
+          getEstimate()
+          setErrors({
+            plan: "",
+            amount: null,
+          })
         })
-        setPopUpState(false)
-        getEstimate()
-        setErrors({
-          plan: "",
-          amount: null,
+        .catch((err) => {
+          console.log(err)
         })
-      })
-      .catch((err)=>{
-        console.log(err)
-      })
     }
   }
   const updateHandler = (e) => {
@@ -112,6 +144,16 @@ export default function Estimate() {
         // handleClick={popUpHandler}
         >
           <div className=' grid grid-cols-1 gap-3 p-4 pb-0'>
+            <div>
+              <label className=" text-primary text-sm font-semibold">Date</label>
+              <div className="pt-1">
+                <DatePicker
+                  id="date" selected={startDate}
+                  onChange={(date, e) => setDate(date, e)}
+                  className="border p-1  w-full h-[36px] text-sm" />
+                {errors.date}
+              </div>
+            </div>
             <div>
               <Input type="text" value={values.plan} handleChange={(e) => handleChange(e)} label="Estimate Plan" name="plan" />
               <div className='text-[#dd0821] text-tiny pt-1'>{errors.plan}</div>
@@ -141,6 +183,7 @@ export default function Estimate() {
           <thead>
             <tr className='bg-primary text-[#fff]'>
               <td className=' px-4 text-left py-2'>S.No</td>
+              <td className=' px-4 text-left py-2'>Date</td>
               <td className=' px-4 text-left py-2'>Estimate Plan</td>
               <td className=' px-4 text-left py-2'>Estimate Amount</td>
               <td className=' px-4 text-left py-2 w-[50px]'>Edit</td>
@@ -153,6 +196,7 @@ export default function Estimate() {
               return (
                 <tr className={`${i % 2 === 1 ? "td" : null} `}>
                   <td className=' px-4 text-text-color text-sm py-2'>{i}</td>
+                  <td className=' px-4 text-text-color text-sm py-2'>{v?.showdate}</td>
                   <td className=' px-4 text-text-color text-sm py-2'>{v?.plan}</td>
                   <td className=' px-4 text-text-color text-sm py-2'>{v?.amount}</td>
                   <td className='h-full flex justify-center px-4 text-text-color text-sm py-3 w-[50px]'><HiOutlinePencilAlt onClick={() => handleEdit(v, i)} className='text-[#50933e] hover:text-[#1e6510] duration-300 hover:cursor-pointer' /></td>
---
+++ src/Pages/Util/time.jsx
@@ -0,0 +1,13 @@
+export const timeStamp= (timeStamp)=>{
+    var a = new Date(timeStamp * 1000);
+  var months = ['Jan','Feb','Mar','Apr','May','Jun','Jul','Aug','Sep','Oct','Nov','Dec'];
+  var year = a.getFullYear();
+  var month = months[a.getMonth()];
+  var date = a.getDate();
+  var hour = a.getHours();
+  var min = a.getMinutes();
+  var sec = a.getSeconds();
+  var time = date + ' ' + month + ' ' + year + ' ' + hour + ':' + min + ':' + sec ;
+  console.log(time)
+  return time;
+}
